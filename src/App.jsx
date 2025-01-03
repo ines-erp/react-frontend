60beb4235f3bfed9d602c8043563012868e4c3ca
Wes Benvindo <wal2.designer@gmail.com>
2025-01-03 20:02:42 +0000

    adding mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import {Container} from "@mui/material";
 
 function App() {
 
-  return (
-    <>
-     APP
-    </>
-  )
+    return (
+        <Container maxWidth={false} sx={{background: "red"}}>
+            app
+        </Container>
+    )
 }
 
 export default App
