f33f20701729d175e8f9cf7a4e342a3948cccd5c
Bruna <brn.flor@gmail.com>
2025-01-04 20:14:42 +0000

    Open and close menu groups

--- src/menu/dynamicMenuItems.jsx
+++ src/menu/dynamicMenuItems.jsx
@@ -9,47 +9,50 @@ import React, {useState} from "react";
  * @returns React.ReactNode
  */
 export const dynamicMenuItems = (routes) => {
-    const [isVisible, setIsVisible] = useState(false);
 
     return routes.map((route) => {
-        return route.children.map((childRoute) => {
-            if (childRoute.isInMenu && childRoute.isEnabled) {
-                const currentPath = `${route.path}/${childRoute.path}`
-
-                switch(!!childRoute.parentLabel){
-                    case true:
-                        return (
-                            <>
-                            <ListItem disablePadding sx={{textDecoration: 'none'}} as={Button} onClick={() => {setIsVisible((prev) => !prev)}}>
-                                <ListItemButton>
-                                    {childRoute.icon && <ListItemIcon>
-                                        {childRoute.icon}
-                                    </ListItemIcon>}
-                                    <ListItemText primary={childRoute.parentLabel}/>
-                                </ListItemButton>
-                            </ListItem>
-                                <ListItem disablePadding as={Link} to={currentPath} sx={{textDecoration: 'none', display: isVisible ? "block" : "none"}}>
-                                    <ListItemButton>
-                                        <ListItemText primary={childRoute.label}/>
-                                    </ListItemButton>
-                                </ListItem>
-                                </>
-                        )
-                    default:
-                        return (
-                            <ListItem disablePadding as={Link} to={currentPath} sx={{textDecoration: 'none', display: isVisible ? "block" : "none"}}>
-                                <ListItemButton>
-                                    {childRoute.icon && <ListItemIcon>
-                                        {childRoute.icon}
-                                    </ListItemIcon>}
-                                    <ListItemText primary={childRoute.label}/>
-                                </ListItemButton>
-                            </ListItem>
-                        )
-                }
-            }
-            return null
-        })
+        return <MenuGroup route={route.children}/>
     })
 }
 
+const MenuGroup = ({route}) => {
+    const [isVisible, setIsVisible] = useState(false);
+    return route.map((childRoute) => {
+        if (childRoute.isInMenu && childRoute.isEnabled) {
+            const currentPath = `${route.path}/${childRoute.path}`
+
+            switch (!!childRoute.parentLabel) {
+                case true:
+                    return (<>
+                        <ListItem disablePadding sx={{textDecoration: 'none'}} as={Button} onClick={() => {
+                            setIsVisible((prev) => !prev)
+                        }}>
+                            <ListItemButton>
+                                {childRoute.icon && <ListItemIcon>
+                                    {childRoute.icon}
+                                </ListItemIcon>}
+                                <ListItemText primary={childRoute.parentLabel}/>
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding as={Link} to={currentPath}
+                                  sx={{textDecoration: 'none', display: isVisible ? "block" : "none"}}>
+                            <ListItemButton>
+                                <ListItemText primary={childRoute.label}/>
+                            </ListItemButton>
+                        </ListItem>
+                    </>)
+                default:
+                    return (<ListItem disablePadding as={Link} to={currentPath}
+                                      sx={{textDecoration: 'none', display: isVisible ? "block" : "none"}}>
+                        <ListItemButton>
+                            {childRoute.icon && <ListItemIcon>
+                                {childRoute.icon}
+                            </ListItemIcon>}
+                            <ListItemText primary={childRoute.label}/>
+                        </ListItemButton>
+                    </ListItem>)
+            }
+        }
+        return null
+    })
+}
--- src/pages/otherModule/otherModuleRoutes.jsx
+++ src/pages/otherModule/otherModuleRoutes.jsx
@@ -1,9 +1,10 @@
 import { Outlet} from "react-router-dom";
 import {OtherModule} from "@/pages/otherModule/index.jsx";
 import {HomePage} from "@/pages/home/index.jsx";
+import {BiUser} from "react-icons/bi";
 
 export const otherRoutesList = [
-    {path: "", element: <OtherModule/>, label:"Other", isInMenu: true, isEnabled: true},
+    {path: "", element: <OtherModule/>, label:"Dashboard Other", isInMenu: true, isEnabled: true, parentLabel:"Jaca Other", icon:<BiUser />},
     {path: "jaca", element: <h1>Finances</h1>, label:"Jaca", isInMenu: true, isEnabled: true},
     {path: "melao", element: <h1>Any other</h1>, label:"Other melao", isInMenu: true, isEnabled: true},
 ]
