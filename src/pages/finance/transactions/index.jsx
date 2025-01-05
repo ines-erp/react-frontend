b182a34b50573b7797d0af236fc8ce47f3aaa7d3
Wes Benvindo <wal2.designer@gmail.com>
2025-01-05 13:10:11 +0000

    Transactions cards and dashboard header

--- src/pages/finance/transactions/index.jsx
+++ src/pages/finance/transactions/index.jsx
@@ -1,10 +1,96 @@
+import {Box, Card, CardContent, Chip, Container, Typography} from "@mui/material";
+import {MonetizationOnOutlined} from "@mui/icons-material";
+import {green, grey} from "@mui/material/colors"
+
 export const TransactionsDashboard = () => {
     return (
-        <>
-            <h1>Transactions</h1>
-            <li><a href="transactions/details">details</a></li>
-            <li><a href="transactions/new">new</a></li>
-        </>
+        <Container maxWidth={false}>
+            <Typography variant={"h1"} gutterBottom fontSize={"3rem"}>Transactions</Typography>
+
+            <Box sx={{display: "flex", gap: "24px", mb: "32px"}}>
+                <Card sx={{maxWidth: "240px", flex: 1, borderRadius: 4, border: "none"}} variant={"outlined"}>
+                    <CardContent>
+                        <Typography variant={"h3"} sx={{
+                            color: 'text.secondary',
+                            fontSize: 14,
+                            alignItems: "center",
+                            display: "flex",
+                            gap: "8px",
+                            mb: "16px"
+                        }}>
+                            <MonetizationOnOutlined/>
+                            Incomes
+                        </Typography>
+
+                        <Typography gutterBottom variant="h5" component="div">
+                            R$ 100,00 <Chip label="12.8%" color="success" size="small"></Chip>
+                        </Typography>
+
+                        <Typography variant="caption">
+                            <Typography variant="string" fontWeight="bold" color="success">+ R$ 30,00</Typography> than
+                            last month
+                        </Typography>
+                    </CardContent>
+                </Card>
+
+                <Card sx={{maxWidth: "240px", flex: 1, borderRadius: 4, border: "none"}} variant={"outlined"}>
+                    <CardContent>
+                        <Typography variant={"h3"} sx={{
+                            color: 'text.secondary',
+                            fontSize: 14,
+                            alignItems: "center",
+                            display: "flex",
+                            gap: "8px",
+                            mb: "16px"
+                        }}>
+                            <MonetizationOnOutlined/>
+                            Outcomes
+                        </Typography>
+
+                        <Typography gutterBottom variant="h5" component="div">
+                            -R$ 30,00 <Chip label="12.8%" color="warning" size="small"></Chip>
+                        </Typography>
+
+                        <Typography variant="caption">
+                            <Typography variant="string" fontWeight="bold" color="warning">- R$ 10,00</Typography> than
+                            last month
+                        </Typography>
+                    </CardContent>
+                </Card>
+
+                <Card sx={{maxWidth: "240px", flex: 1, borderRadius: 4, background: green[50], border: "none"}}
+                      variant={"outlined"}>
+                    <CardContent>
+                        <Typography variant={"h3"} sx={{
+                            color: 'text.secondary',
+                            fontSize: 14,
+                            alignItems: "center",
+                            display: "flex",
+                            gap: "8px",
+                            mb: "16px"
+                        }}>
+                            <MonetizationOnOutlined/>
+                            Balance
+                        </Typography>
+
+                        <Typography gutterBottom variant="h5" component="div">
+                            R$ 70,00 <Chip label="12.8%" color="success" size="small"></Chip>
+                        </Typography>
+
+                        <Typography variant="caption">
+                            <Typography variant="string" fontWeight="bold" color="success">+ R$ 20,00</Typography> than
+                            last month
+                        </Typography>
+                    </CardContent>
+
+                </Card>
+            </Box>
+
+            <div>statistics</div>
+
+            <div>Latest transactions</div>
+
+        </Container>
     )
 
 }
--- src/pages/finance/transactions/transactionsRoutes.jsx
+++ src/pages/finance/transactions/transactionsRoutes.jsx
@@ -3,6 +3,7 @@ import {TransactionsDashboard} from "@/pages/finance/transactions/index.jsx";
 
 export const transactionsRoutesList = [
     {path: "", element: <TransactionsDashboard />, label:"Dashboard trasactions", isInMenu: true, isEnabled: true, parentLabel:"Finance", icon:<BiUser />},
-    {path: "details", element: <h1>Transactions datails</h1>, label:"Details", isInMenu: false, isEnabled: true},
-    {path: "new", element: <h1>Any other</h1>, label:"New transaction", isInMenu: false, isEnabled: false},
+    {path: "add", element: <h1>Any other</h1>, label:"New transaction", isInMenu: false, isEnabled: false},
+    {path: "details/{:id}", element: <h1>Transactions datails</h1>, label:"Details", isInMenu: false, isEnabled: true},
+    {path: "delete/{:id}", element: <h1>Any other</h1>, label:"Delete transaction", isInMenu: false, isEnabled: false},
 ]
