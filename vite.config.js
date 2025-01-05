cabc4dbc392e6177987700cec61436ec7e3edc76
Bruna <brn.flor@gmail.com>
2025-01-05 13:30:56 +0000

    adding proxy with axios

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'), // Replace './src' with your source directory
-      // Add more aliases as needed:
-      // '~': path.resolve(__dirname, './node_modules'),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve('./src'), // Replace './src' with your source directory
+            // Add more aliases as needed:
+            // '~': path.resolve(__dirname, './node_modules'),
+        },
     },
-  },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5047/api/v1',
+                changeOrigin: true,
+                secure: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        }
+    }
 })
