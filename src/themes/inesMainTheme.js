571b67378e001d5cd9f30761101be97e27972f79
Wes Benvindo <wal2.designer@gmail.com>
2025-01-07 14:25:44 +0000

    WIP: adjusting menu theme

--- src/themes/inesMainTheme.js
+++ src/themes/inesMainTheme.js
@@ -1,16 +1,27 @@
 import {createTheme} from "@mui/material";
-import {green, purple} from "@mui/material/colors";
+import {blue, blueGrey, green, grey} from "@mui/material/colors";
 
 export const inesMainTheme = createTheme({
     cssVariables: true,
     palette: {
         primary: {
-            main: purple[500],
+            main: blue[600],
         },
         secondary: {
-            main: green[500],
+            main: green[100],
         },
     },
-    spacing: Array.from({length:10},(_, idx) => (++idx-1)*8),
+    spacing: Array.from({length: 10}, (_, idx) => (++idx - 1) * 8),
     borderRadius: 8,
+    components: {
+        MuiListItemButton: {
+            styleOverrides: {
+                root: {
+                    margin: 8,
+                    borderRadius: 8,
+                }
+            }
+        }
+    }
 })
+
