4807a453728b80c8e43dd59daa8c5b3a05b2b1f7
Bruna <brn.flor@gmail.com>
2025-01-07 18:43:33 +0000

    adding card with modal to edit it

--- src/pages/finance/paymentMethod/index.jsx
+++ src/pages/finance/paymentMethod/index.jsx
@@ -1,7 +1,7 @@
 import {
     Box, Button,
     Card, CardActions,
-    CardContent, Stack,
+    CardContent, Modal, Stack, TextField,
     Typography
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -12,6 +12,7 @@ import {getFromApiData} from "@/api/helpers/getFromApiData.js";
 
 export const PaymentMethodDashboard = () => {
     const [paymentMethods, setPaymentMethods] = useState([])
+
 
     const getPaymentMethods = async () => {
         const data = await getFromApiData("paymentmethod");
@@ -24,6 +25,11 @@ export const PaymentMethodDashboard = () => {
         })
         setPaymentMethods(formatted);
     }
+
+    const handleUpdate = (paymentMethod) => {
+        console.log(paymentMethod)
+    }
+
     useEffect(() => {
         getPaymentMethods();
     }, [])
@@ -34,31 +40,78 @@ export const PaymentMethodDashboard = () => {
                 <Typography variant="h4" component="h1">
                     Payment methods
                 </Typography>
-                <Button variant="contained" startIcon={<Add />}>New</Button>
+                <Button onClick={()=>{console.log("not implemented")}} variant="contained" startIcon={<Add />}>New</Button>
             </Stack>
 
-            <Box sx={{display: "flex", flexDirection: "column", gap: 4}}>
+            <Box sx={{display: "flex", flexDirection: "column", gap: 2}}>
                 {paymentMethods.map((paymentMethod) => {
                     return (
-                    <Card key={paymentMethod.id}>
-                        <CardContent>
-                            <Typography sx={{ color: "text.secondary", mb: 1.5 }}>{paymentMethod.type}</Typography>
-                            <Typography variant="h5" component="div">{paymentMethod.name}</Typography>
-                            <Typography>{paymentMethod.description}</Typography>
-                            <Typography sx={{ color: "text.secondary", mb: 1.5 }}>{paymentMethod.createdAt}</Typography>
-                        </CardContent>
-                        <CardActions sx={{justifyContent:"end"}}>
-                            <Button variant="outlined" size="small" startIcon={<BiPencil />}>Edit</Button>
-                            <Button variant="outlined" size="small" color="error" startIcon={<DeleteIcon />}>
-                                Delete
-                            </Button>
-                        </CardActions>
-                    </Card>
+                    <PaymentMethodsCard key={paymentMethod.id} paymentMethod={paymentMethod}  onUpdate={handleUpdate}/>
                 )})}
             </Box>
-
         </Box>
     )
 }
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    borderRadius: 4,
+    boxShadow: 24,
+    p: 4,
+    display: 'flex',
+    flexDirection: 'column',
+    gap:2
+};
 
+const PaymentMethodsCard = ({paymentMethod, onUpdate }) => {
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        console.log("open", open);
+        setOpen(true)
+    };
+    const handleClose = () => setOpen(false);
+
+    return (
+        <>
+        <Modal
+            open={open}
+            onClose={handleOpen}
+        >
+            <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Edit Payment Method
+                </Typography>
+                <TextField label="Type" variant="outlined" fullWidth/>
+                <TextField label="Name" variant="outlined" fullWidth/>
+                <TextField label="Description" variant="outlined" multiline fullWidth/>
+                <Stack direction="row" spacing={2} justifyContent="end">
+
+                <Button variant="contained" onClick={onUpdate}>Update</Button>
+                    <Button variant="contained" onClick={handleClose} color="error">Cancel</Button>
+                </Stack>
+            </Box>
+        </Modal>
+
+        <Card key={paymentMethod.id}>
+            <CardContent>
+                <Typography sx={{ color: "text.secondary", mb: 1.5 }}>{paymentMethod.type}</Typography>
+                <Typography variant="h5" component="div">{paymentMethod.name}</Typography>
+                <Typography>{paymentMethod.description}</Typography>
+                <Typography sx={{ color: "text.secondary", mb: 1.5 }}>{paymentMethod.createdAt}</Typography>
+            </CardContent>
+            <CardActions sx={{justifyContent:"end"}}>
+                <Button variant="outlined" size="small" startIcon={<BiPencil />} onClick={handleOpen}>Edit</Button>
+                <Button variant="outlined" size="small" color="error" startIcon={<DeleteIcon />}>
+                    Delete
+                </Button>
+            </CardActions>
+        </Card>
+        </>
+    )
+}
