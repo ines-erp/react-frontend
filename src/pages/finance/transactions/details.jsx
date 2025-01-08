0bbeed695a6ae8ce86bcd80dd231945058925bc3
Wes Benvindo <wal2.designer@gmail.com>
2025-01-08 13:57:31 +0000

    WIP: Editing attachments

--- src/pages/finance/transactions/details.jsx
+++ src/pages/finance/transactions/details.jsx
@@ -1,9 +1,21 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {getFromApiData} from "@/api/helpers/getFromApiData.js";
-import {Box, Button, Chip, Container, IconButton, Paper, Typography} from "@mui/material";
-import {grey} from "@mui/material/colors";
-import {ContentCopy} from "@mui/icons-material";
+import {
+    Box,
+    Button,
+    Chip,
+    Container,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText,
+    Paper,
+    Typography
+} from "@mui/material";
+import {blueGrey, grey} from "@mui/material/colors";
+import {ContentCopy, Description, DescriptionOutlined, UploadFile} from "@mui/icons-material";
 
 export const TransactionsDetails = () => {
 
@@ -29,7 +41,7 @@ export const TransactionsDetails = () => {
         const transactionDate = new Date(Date.parse(transaction.date))
 
         return (
-            <Container maxWidth={false}>
+            <Container sx={{ml: 0}}>
 
                 <Typography variant="h1">Transactions details</Typography>
                 <Typography variant="h5" color={grey[500]}>
@@ -48,7 +60,7 @@ export const TransactionsDetails = () => {
                     alignItems: "center",
                     borderBottom: `1px solid ${grey[300]}`
                 }}>
-                    <Box sx={{width: "100%", pr:"32px"}}>
+                    <Box sx={{width: "100%", pr: "32px"}}>
                         <Typography variant="h5"
                                     sx={{
                                         display: "flex",
@@ -57,17 +69,19 @@ export const TransactionsDetails = () => {
                                         mb: 1
                                     }}>
                             <Chip label={transaction.transactionType.name}
-                                  color={(transaction.transactionType.name).toLowerCase() === "income" ? "success" : "warning"}/>
+                                  color={(transaction.transactionType.name).toLowerCase() === "income" ? "success" : "warning"}
+                                  sx={{fontWeight: "bold"}}/>
                             {transactionDate.toLocaleDateString()}
                         </Typography>
                         <Box sx={{display: "flex", justifyContent: "space-between", mb: '0', flex: 1, width: "100%"}}>
-                            <Typography variant={"h2"}>
+                            <Typography variant={"h2"} color={blueGrey[600]}>
                                 {transaction.name}
                             </Typography>
 
-                            <Typography variant={"h2"} sx={{gap: "1rem", display: "flex"}} color={transaction.amount > 0 ? "success":"warning"}>
+                            <Typography variant={"h2"} sx={{gap: "1rem", display: "flex"}}
+                                        color={transaction.amount > 0 ? "success" : "warning"}>
                                 <span>{transaction.currency.symbol}</span>
-                                <span >{transaction.amount.toFixed(2)}</span>
+                                <span>{transaction.amount.toFixed(2)}</span>
                             </Typography>
                         </Box>
                     </Box>
@@ -81,57 +95,72 @@ export const TransactionsDetails = () => {
 
                 </Box>
 
-                <Paper variant={"outlined"} sx={{padding: 4, mt: 4, borderRadius: 1, border: "none"}}>
+                <Paper variant={"outlined"} sx={{padding: 4, mt: 4, borderRadius: 2, border: "none"}}>
 
                     <Box sx={{mb: 4}}>
                         <Typography variant={"h3"} sx={{mb: 4}}>Description</Typography>
                         <Typography variant={"p"} sx={{mb: 4}}>{transaction.description}</Typography>
                     </Box>
-
-                    <Box sx={{display: "flex", gap: 4}}>
-
-                        <Box sx={{mb: 4, maxWidth: "50%", flex: 1}}>
-                            <Typography variant={"h3"} sx={{mb: 4}}>Transaction info</Typography>
-                            <Typography>Paid by: {transaction.paidBy}</Typography>
-                            <Typography>Recieved by: {transaction.recievedBy}</Typography>
-                            <Typography>Transaction type: {transaction.transactionType.name}</Typography>
-                            <Typography>Payment method: {transaction.paymentMethod.name}</Typography>
-                        </Box>
-
-                        <Box sx={{mb: 4, maxWidth: "50%", flex: 1}}>
-                            <Typography variant={"h3"} sx={{mb: 4}}>Currency info</Typography>
-                            <Typography>Currency name: <strong>{transaction.currency.name}</strong></Typography>
-                            <Typography>Currency symbol: <strong>{transaction.currency.symbol}</strong></Typography>
-                            <Typography>Currency code: <strong>{transaction.currency.symbol}</strong></Typography>
-                            <Typography>amount: <strong>{transaction.amount}</strong></Typography>
-                        </Box>
-
-                    </Box>
-
                 </Paper>
+
+                <Box sx={{display: "flex", gap: 4}}>
+                    <Paper variant={"outlined"}
+                           sx={{padding: 4, mt: 4, borderRadius: 2, border: "none", mb: 4, maxWidth: "50%", flex: 1}}>
+                        <Typography variant={"h3"} sx={{mb: 4}}>Transaction info</Typography>
+                        <Typography>Paid by: {transaction.paidBy}</Typography>
+                        <Typography>Recieved by: {transaction.recievedBy}</Typography>
+                        <Typography>Transaction type: {transaction.transactionType.name}</Typography>
+                        <Typography>Payment method: {transaction.paymentMethod.name}</Typography>
+                    </Paper>
+
+                    <Paper variant={"outlined"}
+                           sx={{padding: 4, mt: 4, borderRadius: 2, border: "none", mb: 4, maxWidth: "50%", flex: 1}}>
+                        <Typography variant={"h3"} sx={{mb: 4}}>Currency info</Typography>
+                        <Typography>Currency name: <strong>{transaction.currency.name}</strong></Typography>
+                        <Typography>Currency symbol: <strong>{transaction.currency.symbol}</strong></Typography>
+                        <Typography>Currency code: <strong>{transaction.currency.symbol}</strong></Typography>
+                        <Typography>amount: <strong>{transaction.amount}</strong></Typography>
+                    </Paper>
+                </Box>
 
 
                 <Paper variant={"outlined"} sx={{padding: 4, mt: 4, borderRadius: 1, border: "none"}}>
                     <Box sx={{mb: 4}}>
-                        <Typography variant={"h3"} sx={{mb: 4}}>Atachments</Typography>
+                        <Typography variant={"h3"} sx={{mb: 4}}>Attachments</Typography>
 
-                        <ul>
-                            <li>Atatchment 01</li>
-                            <li>Atatchment 02</li>
-                        </ul>
+                        <List sx={{display: "flex", justifyContent: "start"}}>
+                            <ListItem sx={{flex: 1, maxWidth: "25%", minWidth: "200px"}}>
+                                <DescriptionOutlined sx={{fontSize: "48px"}}/>
+                                File name
+                            </ListItem>
+
+                            {/*//TODO: CREATE THE STYLE TO CONFIG HOVER FOR THAT*/}
+                            <ListItemButton sx={{maxWidth:"200px", border: `4px dashed ${grey[300]}`, color:grey[300], paddingY:"16px"}}>
+                                <UploadFile sx={{fontSize: "48px"}}/>
+                                Add new attachment
+                            </ListItemButton>
+                        </List>
                     </Box>
                 </Paper>
 
-                <Paper variant={"outlined"} sx={{padding: 4, mt: 4, borderRadius: 1, border: "none"}}>
-                    <Box sx={{mb: 4}}>
-                        <Typography variant={"h3"} sx={{mb: 4}}>Logs</Typography>
+                <Box variant={"outlined"} sx={{
+                    padding: 4,
+                    mt: 4,
+                    borderRadius: 1,
+                    border: "none",
+                    borderTop: `1px solid ${grey[300]}`,
+                    opacity: .6
+                }}>
+                    <Typography variant={"h4"} sx={{mb: 2}}>Logs</Typography>
 
-                        <ul>
-                            <li>{transaction.creteateAt}</li>
-                            <li>{transaction.updateAt}</li>
-                        </ul>
-                    </Box>
-                </Paper>
+                    <List>
+                        <ListItem sx={{fontWeight: "bold"}}>
+                            <Typography variant={"h5"}>
+                                Jaca element
+                            </Typography>
+                        </ListItem>
+                    </List>
+                </Box>
 
 
             </Container>
