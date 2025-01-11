c47d268f0b1eb1cdc291bfb35c71651342ddfc3e
Wes Benvindo <wal2.designer@gmail.com>
2025-01-11 14:15:16 +0000

    Updating the routes and removing some trash, fixing deleting routing as well

--- src/pages/finance/transactions/details.jsx
+++ src/pages/finance/transactions/details.jsx
@@ -15,7 +15,7 @@ import {
 import {blue, blueGrey, grey} from "@mui/material/colors";
 import {ArrowBack, ContentCopy, DescriptionOutlined, NavigateBeforeRounded, UploadFile} from "@mui/icons-material";
 import {NewTransactionModal} from "@/pages/finance/transactions/newTransactionModal.jsx";
-import {getFromApiData, putToApiData} from "@/api/inesDataApiV1.js";
+import {DeleteFromApiData, getFromApiData, putToApiData} from "@/api/inesDataApiV1.js";
 
 
 const CATEGORIES = [
