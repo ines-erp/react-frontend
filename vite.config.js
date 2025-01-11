811ecaa5d5b78d8f04edd6f2db29ff618b2f2d9f
Wes Benvindo <wal2.designer@gmail.com>
2025-01-11 15:34:43 +0000

    Creating some eslint rules

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), eslint()],
     resolve: {
         alias: {
             '@': path.resolve('./src'), // Replace './src' with your source directory
