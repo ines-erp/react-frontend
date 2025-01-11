590034213f9f3c690f052211eac84380beea9ba3
Wes Benvindo <wal2.designer@gmail.com>
2025-01-11 12:41:36 +0000

    Destructing some variables from transactions

--- src/pages/finance/transactions/index.jsx
+++ src/pages/finance/transactions/index.jsx
@@ -59,7 +59,7 @@ export const TransactionsDashboard = () => {
 
     const [transactions, setTransactions] = useState([])
     const [balance, setBalance] = useState({})
-    const [currency, setCurrency] = useState("Brazilian Real")
+    const [currency, setCurrency] = useState("BRL")
 
     //modal
     const [open, setOpen] = useState(false);
@@ -92,6 +92,9 @@ export const TransactionsDashboard = () => {
     const handleGetTransactionsAndBalances = async (currency) => {
         const transactionsResponse = await getFromApiData('transactions?' + new URLSearchParams({currency: currency}).toString())
         const balanceResponse = await getFromApiData('balance?' + new URLSearchParams({currency: currency}).toString())
+
+        // const transactionsResponse = await getFromApiData('transactions')
+        // const balanceResponse = await getFromApiData('balance')
 
         Promise.all([transactionsResponse, balanceResponse])
             .then(([transactions, balance]) => {
@@ -156,7 +159,7 @@ export const TransactionsDashboard = () => {
                     <Chip label={`${option.label} - ${option.name}`} sx={{fontWeight: "bold"}} size={"medium"}
                           key={option.value}
                           value={option.name} color={currency === option.name ? "primary" : ""} onClick={() => {
-                        handleSelectCurrency(option.name)
+                        handleSelectCurrency(option.value)
                     }}/>
                 ))}
             </Box>
@@ -269,25 +272,87 @@ export const TransactionsDashboard = () => {
 
                 <Box sx={{display: "flex", gap: "16px", flexDirection: "column"}}>
                     {transactions.map(transaction => {
+                        const {
+                            id,
+                            name,
+                            description,
+                            amount,
+                            date,
+                            createdAt,
+                            updatedAt,
+                            paidBy,
+                            receivedBy
+                        } = transaction;
+
+                        const {id: transactionTypeId, name: transactionTypeName} = transaction.transactionType
+
+                        const {id: categoryId, name: categoryName} = transaction.transactionCategory
+
+                        const {
+                            id: pymentMethodId,
+                            type: pymentMethodType,
+                            name: pymentMethodName,
+                            description: pymentMethodDescription,
+                            createdAt: pymentMethodCreatedAt,
+                            updatedAt: pymentMethodUpdatedAt
+                        } = transaction.paymentMethod
+
+                        const {
+                            id: currencyId,
+                            name: currencyName,
+                            symbol: currencySymbol,
+                            isoCode: currencyIsoCode
+                        } = transaction.paymentMethod.currency
+
+
                         return (
-                            <Card variant={"outlined"} key={transaction.id}
-                                  sx={{background: transaction.transactionType.name.toLowerCase() === "income" ? "#00ff0009" : "#ff000009"}}>
+                            <Card
+                                variant={"outlined"}
+                                key={transaction.id}
+                                sx={{background: transaction.transactionType.name.toLowerCase() === "income" ? "#00ff0009" : "#ff000009"}}
+                            >
+
                                 <CardContent
-                                    sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-                                    <Typography variant={"h3"} fontSize={"1rem"}>{transaction.name}</Typography>
-                                    <Typography>{transaction.transactionType.name}</Typography>
-                                    <Typography>{transaction.currency.symbol}: {transaction.amount.toFixed(2)}</Typography>
-                                    <Typography>{transaction.description.substring(0, 30) + "..."}</Typography>
+                                    sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}
+                                >
+
+                                    <Typography variant={"h3"} fontSize={"1rem"}>
+                                        {name}
+                                    </Typography>
+
+                                    <Typography>
+                                        {transactionTypeName}
+                                    </Typography>
+                                    <Typography>
+                                        {currencySymbol}: {amount.toFixed(2)}
+                                    </Typography>
+
+                                    <Typography>
+                                        {description.substring(0, 30) + "..."}
+                                    </Typography>
 
                                     <Box sx={{display: "flex", gap: "8px", alignItems: "center", my: "auto"}}>
-                                        <Button as={Link} variant={"outlined"} sx={{background: "#fff"}}
-                                                to={`details/${transaction.id}`}>Details</Button>
-                                        <Button componet={Link} variant={"outlined"} color={"error"}
-                                                sx={{background: "#fff"}}
-                                                onClick={() => handleDelete(transaction.id)}>Delete</Button>
+                                        <Button
+                                            as={Link}
+                                            variant={"outlined"}
+                                            sx={{background: "#fff"}}
+                                            to={`details/${id}`}
+                                        >
+                                            Details
+                                        </Button>
+
+                                        <Button
+                                            componet={Link}
+                                            variant={"outlined"}
+                                            color={"error"}
+                                            sx={{background: "#fff"}}
+                                            onClick={() => handleDelete(id)}
+                                        >
+                                            Delete
+                                        </Button>
                                     </Box>
+
                                 </CardContent>
-                                <Box></Box>
                             </Card>
                         )
                     })}
