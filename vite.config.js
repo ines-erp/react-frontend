2f6c671e30b9a38676dc1cedd4edf5ccfc302770
Wes Benvindo <wal2.designer@gmail.com>
2025-01-12 15:46:11 +0000

    updating vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,33 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), eslint()],
-    resolve: {
-        alias: {
-            '@': path.resolve('./src'), // Replace './src' with your source directory
-            // Add more aliases as needed:
-            // '~': path.resolve(__dirname, './node_modules'),
-        },
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:5047/api/v1',
-                changeOrigin: true,
-                secure: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({mode}) => {
+    return {
+        plugins: [react(), eslint({
+            exclude: ["**/.config"],
+            failOnError: mode !== "development",
+            lintOnStart: mode !== "development",
+            cache: true
+        })],
+        resolve: {
+            alias: {
+                '@': path.resolve('./src'), // Replace './src' with your source directory
+                // Add more aliases as needed:
+                // '~': path.resolve(__dirname, './node_modules'),
             },
+        },
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:5047/api/v1',
+                    changeOrigin: true,
+                    secure: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                },
+            }
         }
-    }
-})
+    };
+});
