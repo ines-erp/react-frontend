9f09757d6e805393180133e8e5f8ab11a824a845
Wes Benvindo <wal2.designer@gmail.com>
2025-01-13 14:53:41 +0000

    lint changes and config

--- src/pages/home/homeRoutes.jsx
+++ src/pages/home/homeRoutes.jsx
@@ -4,9 +4,32 @@ import {MailLock} from "@mui/icons-material";
 
 //example
 export const homeRoutesList = [
-    {path: "", element: <HomePage/>, label:"Dashboard", isInMenu: true, isEnabled: true, icon: <MailLock />, parentLabel:"Home"},
-    {path: "finance", element: <h1>Finances</h1>, label:"Finance", isInMenu: true, isEnabled: false },
-    {path: "payment", element: <h1>Any payment</h1>, label:"payment home", isInMenu: false, isEnabled: true},
+    {
+        path: "",
+        element: <HomePage/>,
+        label: "Dashboard",
+        isInMenu: true,
+        isEnabled: true,
+        icon: <MailLock/>,
+        parentLabel: "Home",
+        namespace: "home"
+    },
+    {
+        path: "finance",
+        element: <h1>Finances</h1>,
+        label: "Finance",
+        isInMenu: true,
+        isEnabled: false,
+        namespace: "finance"
+    },
+    {
+        path: "payment",
+        element: <h1>Any payment</h1>,
+        label: "payment home",
+        isInMenu: false,
+        isEnabled: true,
+        namespace: "payment"
+    },
 ]
 
 export const homeRoutes = [
@@ -17,3 +40,7 @@ export const homeRoutes = [
     }
 ]
 
+//
+
+// func for route in routes recursive [namespace:namesapce:namespace]
+// func that will split that up list and convert to url pattern
