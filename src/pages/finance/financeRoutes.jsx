0c9efe9a4e4e8e9d63d583d927f0f50046faadd6
Wes Benvindo <wal2.designer@gmail.com>
2025-01-13 10:52:56 +0000

    wip: waiting for modularization
    (cherry picked from commit 156465a7c6cf3cbe20dae12308a5682a1ed67e84)

--- src/pages/finance/financeRoutes.jsx
+++ src/pages/finance/financeRoutes.jsx
@@ -1,9 +1,10 @@
-import {Outlet} from "react-router-dom";
 import {BiUser} from "react-icons/bi";
 import {FinancesDashboard} from "@/pages/finance/index.jsx";
-import {transactionsRoutesList} from "@/pages/finance/transactions/transactionsRoutes.jsx";
+import {Outlet} from "react-router-dom";
 import {paymentMethodsRoutesList} from "@/pages/finance/paymentMethod/paymentMethodsRoutes.jsx";
-import {LayoutMenuAppbar} from "@/layouts/main/LayoutMenuAppbar.jsx";
+import {transactionsRoutesList} from "@/pages/finance/transactions/transactionsRoutes.jsx";
+import {transactionCategoryRoutes} from "@/pages/finance/category/transactionCategoryRoutes.jsx";
+import {transactionTypesRoutes} from "@/pages/finance/transactionTypes/transactionTypesRoutes.jsx";
 
 export const financesRoutesList = [
     {
@@ -29,14 +30,30 @@ export const financesRoutesList = [
         isEnabled: true,
         isInMenu: true,
         label: "Payment methods",
-        path: "paymentmethods"
+        path: "payment-methods"
+    },
+    {
+        children: transactionCategoryRoutes.filter(item => item.isEnabled === true),
+        element: <Outlet/>,
+        isEnabled: true,
+        isInMenu: true,
+        label: "Categories",
+        path: "categories"
+    },
+    {
+        children: transactionTypesRoutes.filter(item => item.isEnabled === true),
+        element: <Outlet/>,
+        isEnabled: true,
+        isInMenu: true,
+        label: "Transaction Types",
+        path: "transaction-types"
     }
 ];
 
 export const financesRoutes = [
     {
         children: financesRoutesList.filter(item => item.isEnabled === true),
-        element: <LayoutMenuAppbar/>,
+        element: <Outlet/>,
         path: "/finance"
     }
 ];
