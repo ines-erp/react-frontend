3e18424560703295fd4538bc03964bc37c2f92a1
Wes Benvindo <wal2.designer@gmail.com>
2025-01-18 12:20:47 +0000

    WIP: starting with login data

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,20 +1,34 @@
 import {Box, Button, Container, Paper, TextField, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
+import {useRef} from "react";
+import {loginToApi} from "@/api/inesAuthApiV1.js";
 
 export const LoginPage = () => {
+
+    const userName = useRef();
+    const password = useRef();
+
+    const handleLogin = async () => {
+        // http://localhost:5047/api/v1/Auth/Login 
+        const loginData = {"password": password.current.value, "username": userName.current.value};
+        const isLogged = await loginToApi(loginData);
+        console.debug(isLogged);
+
+    };
+
     return (
         <Container sx={{padding: 4}}>
             <Typography variant={"h1"} mb={4} textAlign={"center"}>INES ERP</Typography>
 
-            <Paper sx={{minWidth: "350px", padding: 4, maxWidth: "45%", mx: "auto"}}>
+            <Paper sx={{maxWidth: "45%", minWidth: "350px", mx: "auto", padding: 4}}>
                 <Typography variant={"h2"} mb={2}>Please Login</Typography>
 
                 <Box mb={2}>
-                    <TextField label="username" type="email" fullWidth={true}/>
+                    <TextField name="username" inputRef={userName} label="username" type="email" fullWidth={true}/>
                 </Box>
 
                 <Box mb={2}>
-                    <TextField label="password" type="password" fullWidth={true}/>
+                    <TextField name="password" inputRef={password} label="password" type="password" fullWidth={true}/>
                 </Box>
 
                 <Typography component={Link} to={"/auth/request-new-password"}>
@@ -22,7 +36,7 @@ export const LoginPage = () => {
                 </Typography>
 
                 <Box sx={{display: "flex", justifyContent: "end"}}>
-                    <Button variant={"contained"}>Login</Button>
+                    <Button variant={"contained"} onClick={handleLogin}>Login</Button>
                 </Box>
             </Paper>
         </Container>
