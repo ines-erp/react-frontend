3a8eddc821aea145c18b435d5073f68a66169b80
Bruna <brn.flor@gmail.com>
2025-02-08 13:41:55 +0000

    updating modal texts

--- src/pages/finance/transactions/newTransactionModal.jsx
+++ src/pages/finance/transactions/newTransactionModal.jsx
@@ -94,14 +94,10 @@ export const NewTransactionModal = ({
 
         >
             <DialogTitle variant={"h3"}>
-                Add new transaction
+                {isEditing ? "Editing" : "Adding new"} transaction
             </DialogTitle>
 
             <DialogContent sx={{display: "flex", flexDirection: "column", gap: 4}}>
-                <DialogContentText sx={{mb: 3}}>
-                    Some example text
-                </DialogContentText>
-
                 <FormControl>
                     <FormLabel>Transaction type</FormLabel>
 
